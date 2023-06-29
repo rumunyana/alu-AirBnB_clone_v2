ef4139e68243938fd4fa0d3b9625514213508f40
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-06-29 13:03:11 +0200

    deploy

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def num_route(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<n>', strict_slashes=False)
 def num_route_template(n):
     """Comment"""
     return render_template('5-number.html', n=n)
@@ -45,3 +45,4 @@ def num_route_template(n):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
