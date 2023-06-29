bd1ed2dd4c95f40b661e54a3a43e002af5b72835
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-06-29 13:07:22 +0200

    reforms

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,62 @@
 #!/usr/bin/python3
+"""
+    python script that starts a Flask web application
+"""
 
-"""Script that starts a Flask web application"""
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Comment"""
-    return "Hello HBNB!"
+@app.route('/')
+def hello():
+    """
+        Return: string when route queried
+    """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Comment"""
-    return "HBNB"
+    """
+        Return: string when route queried
+    """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def text_route(text):
-    """Comment"""
-    return "C {}".format(text.replace("_", " "))
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """
+        Return: reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def text_route_python(text="is cool"):
-    """Comment"""
-    return "Python {}".format(text.replace("_", " "))
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """
+        Return: formatted text based on optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-    """Comment"""
-    return "{} is a number".format(n)
+@app.route('/number/<int:n>')
+def number(n=None):
+    """
+        Return: string if n is an integer
+    """
+    return str(n) + ' is a number'
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
-def num_route_template(n):
-    """Comment"""
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """
+        Return: HTML page if n is an integer
+    """
+    path = '5-number.html'
+    return render_template(path, n=n)
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
-
